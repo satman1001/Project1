318134174858549842e35713bbcc0aa9949d817c
satman1001 <sathishcs@msn.com>
2017-07-21 15:02:41 -0700

    c

--- Sourcefile.R
+++ Sourcefile.R
@@ -6,10 +6,23 @@
 # install.packages('dplyr') # data manipulation
 # install.packages('mice') # imputation
 # install.packages('randomForest') # classification algorithm
+# install.packages('htmlwidgets') # interactive widgets
 
-library('ggplot2') # visualization
-library('ggthemes') # visualization
-library('scales') # visualization
-library('dplyr') # data manipulation
-library('mice') # imputation
-library('randomForest') # classification algorithm
+
+
+# library('ggplot2') # visualization
+# library('ggthemes') # visualization
+# library('scales') # visualization
+# library('dplyr') # data manipulation
+# library('mice') # imputation
+# library('randomForest') # classification algorithm
+# library('htmlwidgets') #interactive widgets
+
+train <- read.csv('train.csv', stringsAsFactors = F)
+test  <- read.csv('test.csv', stringsAsFactors = F)
+
+full  <- bind_rows(train, test) # combine
+
+# check data
+str(full)
+summary(full)
