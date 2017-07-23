b101f9e4d67d41815d0e364f31582fcf07004431
satman1001 <sathishcs@msn.com>
2017-07-23 11:38:58 -0700

    c

--- Sourcefile.R
+++ Sourcefile.R
@@ -7,16 +7,17 @@
 # install.packages('mice') # imputation
 # install.packages('randomForest') # classification algorithm
 # install.packages('htmlwidgets') # interactive widgets
+# install.packages("knitr")
 
 
-
-# library('ggplot2') # visualization
-# library('ggthemes') # visualization
-# library('scales') # visualization
-# library('dplyr') # data manipulation
-# library('mice') # imputation
-# library('randomForest') # classification algorithm
-# library('htmlwidgets') #interactive widgets
+ library('ggplot2') # visualization
+ library('ggthemes') # visualization
+ library('scales') # visualization
+ library('dplyr') # data manipulation
+ library('mice') # imputation
+ library('randomForest') # classification algorithm
+ library('htmlwidgets') #interactive widgets
+ library('knitr') #knitr codechunks
 
 train <- read.csv('train.csv', stringsAsFactors = F)
 test  <- read.csv('test.csv', stringsAsFactors = F)
